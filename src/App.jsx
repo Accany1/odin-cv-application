f80b0789f4abee218d999c7b86bb13e29b07d461
Accany1 <129660499+Accany1@users.noreply.github.com>
2025-02-14 21:36:52 +0800

    formatted education for results page

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function SectionInput({name, type, setFunction, label}) {
   )
 }
 
-function EducationInput({setSchool,school,label,title,year,setYear,setTitle, setDelete}) {
+function EducationInput({setSchool,school,label,title,startDate,endDate,setStartDate,setEndDate,setTitle, setDelete, description, setDescription}) {
   return (
       <div className="education-container">
         <div className="education-holding">
@@ -26,20 +26,37 @@ function EducationInput({setSchool,school,label,title,year,setYear,setTitle, set
           <input type="text" name={label} value={title} onChange={setTitle} />
         </div>
         <div className="education-holding">
-          <label htmlFor={label}>Year of study: </label>
-          <input type="number" name={label} value={year} onChange={setYear} />
+          <label htmlFor={label}>Start Date: </label>
+          <input type="date" name={label} value={startDate} onChange={setStartDate} />
+        </div>
+        <div className="education-holding">
+          <label htmlFor={label}>End Date: </label>
+          <input type="date" name={label} value={endDate} onChange={setEndDate} />
+        </div>
+        <div className="education-holding">
+          <label htmlFor={label}>Description: </label>
+          <textarea name={label} value={description} onChange={setDescription} />
         </div>
         <button className="delete-button" onClick={() => setDelete()}>Delete</button>
       </div>
   )
 }
 
-function EducationOutput({school, title, date}) {
+function EducationOutput({school, title, startDate, endDate, description}) {
   return (
     <div className="education-output">
-      <p>{school}</p>
-      <p>{title}</p>
-      <p>{date}</p>
+      <div className="result-holding">
+        <div className='school-title'>{school}</div>
+      </div>
+      <div className="result-holding">
+        <div className='study-title'>{title}</div>
+      </div>
+      <div className="result-holding">
+        <div className='date-title'>{startDate} - {endDate}</div>
+      </div>
+      <div className="result-holding">
+        <div className='description'>{description}</div>
+      </div>
     </div>
   )
 }
@@ -49,15 +66,21 @@ function App() {
   const [email, setEmail] = useState('')
   const [phone, setPhone] = useState('')
   const [schoolID, setSchoolID] = useState(0)
+  const [educationVisibility, setEducationVisibility] = useState('none')
   const [education, setEducation] = useState([])
+  const [experience, setExperience] = useState([])
 
   function EducationAdd() {
-    setEducation([...education, {id: schoolID,school:'', title:'', year:''}])
+    setEducation([...education, {id: schoolID,school:'', title:'', startDate:'', endDate:'', description:''}])
+    setEducationVisibility('block')
     setSchoolID(schoolID + 1)
   }
 
   function EducationDelete(index) {
     setEducation(education.filter((e) => e.id !== index))
+    if (education.length === 0) {
+      setEducationVisibility('none')
+    }
   }
 
   const HandleSchoolChange = (a, e) => {
@@ -84,18 +107,41 @@ function App() {
     return newEducation
   }
 
-  const HandleYearChange = (a, e) => {
+  const HandleStartDateChange = (a, e) => {
     const newData = a
     let newEducation = [...education]
     
     for (let i = 0; i < newEducation.length; i++) {
       if (newEducation[i].id === e.id) {
-        newEducation[i].year = newData
+        newEducation[i].startDate = newData
       }
     }
     return newEducation
   }
 
+  const HandleEndDateChange = (a, e) => {
+    const newData = a
+    let newEducation = [...education]
+    
+    for (let i = 0; i < newEducation.length; i++) {
+      if (newEducation[i].id === e.id) {
+        newEducation[i].endDate = newData
+      }
+    }
+    return newEducation
+  }
+
+  const HandleDescriptionChange = (a, e) => {
+    const newData = a
+    let newEducation = [...education]
+    
+    for (let i = 0; i < newEducation.length; i++) {
+      if (newEducation[i].id === e.id) {
+        newEducation[i].description = newData
+      }
+    }
+    return newEducation
+  }
 
   return (
     <>
@@ -108,17 +154,26 @@ function App() {
             <SectionInput name={phone} label='Phone: ' type="number" setFunction={(e) => setPhone(e.target.value)} />
             <h1>Education</h1>
             <div className="education-topline"></div>
-            {/* <EducationInput school={education.school} label="school" title={education.title} year={education.year} setSchool={(e) => setEducation({...education, school:e.target.value})} setTitle={(e) => setEducation({...education, title:e.target.value})} setYear={(e) => setEducation({...education, year:e.target.value})} /> */}
-            {education.map((e) => {return <EducationInput key={e.id} school={e.school} label="school" title={e.title} year={e.year} setSchool={(a) => setEducation(HandleSchoolChange(a.target.value,e))} setTitle={(a) => setEducation(HandleTitleChange(a.target.value,e))} setYear={(a) => setEducation(HandleYearChange(a.target.value,e))} setDelete={() => EducationDelete(e.id)} />})}
+            {education.map((e) => {return <EducationInput key={e.id} school={e.school} label="school" title={e.title} startDate={e.startDate} endDate={e.endDate} description={e.description} setSchool={(a) => setEducation(HandleSchoolChange(a.target.value,e))} setTitle={(a) => setEducation(HandleTitleChange(a.target.value,e))} setStartDate={(a) => setEducation(HandleStartDateChange(a.target.value,e))} setEndDate={(a) => setEducation(HandleEndDateChange(a.target.value,e))} setDescription={(a) => setEducation(HandleDescriptionChange(a.target.value,e))} setDelete={() => EducationDelete(e.id)} />})}
             <div className="add-education" onClick={EducationAdd}>+ Add education</div>
           </form>
         </div>
         <div className="result-container">
           <h1>Result</h1>
           <div className="result-page">
-            <h1>{name}</h1>
-            <p>{email}</p>
-            <p>{phone}</p>
+            <div className="personal-output">
+                <h1>{name}</h1>
+              <div className="personal-holding">
+                <div>{email.length > 0 && 'Email:'}</div>
+                <div>{email}</div>
+              </div>
+              <div className="personal-holding">
+                <div>{phone.length > 0 && 'Phone:'}</div>
+                <div>{phone}</div>
+              </div>
+            </div>
+            <h2> {educationVisibility === 'block' && 'Education'}</h2>
+            {education.map((e) => {return <EducationOutput key={e.id} school={e.school} title={e.title} startDate={e.startDate} endDate={e.endDate} description={e.description} />})}
           </div>
         </div>
       </div>
